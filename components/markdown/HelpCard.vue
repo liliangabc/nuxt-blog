55da2140474d5eb40a08ce6914080461fe378939
jser.li <jser.li@circcus.com>
2018-12-16 15:36:55 +0800

    完善文章编辑页面

---
+++ components/markdown/HelpCard.vue
@@ -0,0 +1,62 @@
+<template>
+<div class="px-2 py-2 com-markdown-helper">
+  <v-card class="help-card">
+    <v-card-title><h4>快捷键提示</h4></v-card-title>
+    <v-divider></v-divider>
+    <v-list dense>
+      <v-list-tile v-for="(item, index) in items" :key="index">
+        <v-list-tile-content><b>{{item.title}}</b></v-list-tile-content>
+        <v-list-tile-content class="align-end">{{item.content}}</v-list-tile-content>
+      </v-list-tile>
+    </v-list>
+  </v-card>
+</div>
+</template>
+<script>
+const items = [
+  { title: '**粗体**', content: 'ctrl + b' },
+  { title: '*斜体*', content: 'ctrl + i' },
+  { title: '# 一级标题', content: 'ctrl + 1' },
+  { title: '## 二级标题', content: 'ctrl + 2' },
+  { title: '### 三级标题', content: 'ctrl + 3' },
+  { title: '#### 四级标题', content: 'ctrl + 4' },
+  { title: '##### 五级标题', content: 'ctrl + 5' },
+  { title: '###### 六级标题', content: 'ctrl + 6' },
+  { title: '++下划线++', content: 'ctrl + u' },
+  { title: '~~中划线~~', content: 'ctrl + shift + d' },
+  { title: '==标记==', content: 'ctrl + m' },
+  { title: '^上角标^', content: 'ctrl + alt + s' },
+  { title: '~下角标~', content: 'ctrl + shift + s' },
+  { title: '居左对齐', content: 'ctrl + shift + l' },
+  { title: '居中对齐', content: 'ctrl + shift + c' },
+  { title: '居右对齐', content: 'ctrl + shift + r' },
+  { title: '> 段落引用', content: 'ctrl + q' },
+  { title: '有序列表', content: 'ctrl + o' },
+  { title: '无序列表', content: 'ctrl + alt + u' },
+  { title: '[描述](链接)', content: 'ctrl + l' },
+  { title: '代码块', content: 'ctrl + alt + c' },
+  { title: '表格', content: 'ctrl + alt + t' },
+  { title: '上一步', content: 'ctrl + z' },
+  { title: '下一步', content: 'ctrl + y' },
+  { title: '清空', content: 'ctrl + backspace' }
+]
+export default {
+  data() {
+    return {
+      items
+    }
+  }
+}
+</script>
+<style lang="less">
+.com-markdown-helper {
+  min-width: 256px;
+  height: 100%;
+  overflow: auto;
+  .v-list--dense {
+    .v-list__tile {
+      height: 24px;
+    }
+  }
+}
+</style>
--- pages/editor/_id.vue
+++ pages/editor/_id.vue
@@ -1,19 +1,57 @@
 <template>
-<mavon-editor class="page-editor" ref="editor" 
-  :boxShadow="false" :toolbarsFlag="true" :toolbars="MEOptions.toolbars"></mavon-editor>
+<div class="page-editor">
+  <!-- <com-help-card></com-help-card> -->
+  <div class="editor-wrapper">
+    <div class="px-3 titlebar">
+      <v-text-field class="block font-weight-bold headline" label="输入文章标题..." hide-details single-line full-width></v-text-field>
+      <!-- 上传海报 -->
+      <v-menu v-model="posterMenuVisible" :close-on-content-click="false" transition="slide-y-transition" min-width="300" offset-y>
+        <v-btn icon slot="activator">
+          <v-icon color="grey">add_photo_alternate</v-icon>
+        </v-btn>
+        <div class="white px-3 py-3">
+          <div class="subheading font-weight-bold mb-2 grey--text">添加封面大图</div>
+          <v-img :aspect-ratio="16/9" src="https://cdn.vuetifyjs.com/images/parallax/material.jpg">
+            <!-- <v-btn style="height: 100%;" class="my-0 btn-add-poster" block depressed>
+              点击此处添加图片
+            </v-btn> -->
+            <v-layout justify-end>
+              <v-btn title="移除这张图片" class="mx-0 my-0" icon>
+                <v-icon dark>delete_forever</v-icon>
+              </v-btn>
+            </v-layout>
+          </v-img>
+        </div>
+      </v-menu>
+      <!-- 发布 -->
+      <v-btn flat color="info">
+        <v-icon>publish</v-icon>
+        发布文章
+      </v-btn>
+    </div>
+    <mavon-editor class="md-editor" ref="editor" :boxShadow="false" :toolbarsFlag="true" :toolbars="md.toolbars" @save="handleSave"></mavon-editor>
+  </div>
+</div>
 </template>
 <script>
+import ComHelpCard from '~/components/markdown/HelpCard'
 export default {
   layout: 'editor',
+  components: { ComHelpCard },
   data() {
     return {
-      MEOptions: {}
+      md: {},
+      posterMenuVisible: false
+    }
+  },
+  methods: {
+    handleSave(a, b) {
+      console.log(a, b)
     }
   },
   mounted() {
     const { editor } = this.$refs
-    editor.s_fullScreen = true
-    this.MEOptions = {
+    this.md = {
       toolbars: {
         ...editor.toolbars,
         ...{
@@ -21,8 +59,7 @@ export default {
           subfield: false,
           preview: false,
           navigation: false,
-          readmodel: false,
-          save: false
+          readmodel: false
         }
       }
     }
@@ -31,7 +68,23 @@ export default {
 </script>
 <style lang="less">
 .page-editor {
-  width: 100%;
-  height: 100%;
+  height: 100vh;
+  display: flex;
+  .titlebar {
+    display: flex;
+    align-items: center;
+    height: 64px;
+    background-color: #fff;
+  }
+  .v-note-wrapper {
+    z-index: 1;
+  }
+  .editor-wrapper {
+    flex: 1;
+  }
+  .md-editor {
+    overflow: auto;
+    height: calc(100% - 64px);
+  }
 }
 </style>
